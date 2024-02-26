980a829e7ef5d14ad737b58995b0a1f56b997cb2
Joseph Atkins-Turkish <spacerat3004@gmail.com>
2024-02-26 11:54:03 -0800

    a todos app

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,88 @@
+
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+  import Todo from './Todo.svelte';
+
+	import { fade, slide, crossfade } from 'svelte/transition';
+	import { flip } from 'svelte/animate';
+
+	let lastId = 0;
+
+	/** @type {{name: string, completed: boolean, id: number}[]} */
+	let todos = [
+		{ name: 'A todo item', completed: false, id: -1 },
+		{ name: "Animations are fun", completed: false, id: -2 },
+		{ name: 'Whee', completed: false, id: -3 }
+	];
+
+	let newTodo = '';
+
+	function addTodo() {
+		lastId += 1
+		todos = [...todos, { name: newTodo, completed: false, id: lastId }];
+		newTodo = '';
+	}
+
+	$: outstanding = todos.filter(x => !x.completed)
+	$: completed = todos.filter(x => !!x.completed)
+
+	const [send, receive] = crossfade({
+		fallback: (node) => fade(node)
+	})
 </script>
 
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
+<h1>Svelte todos and animations and stuff</h1>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
 
-		to your new<br />SvelteKit app
-	</h1>
+<form on:submit|preventDefault={addTodo} >
+	<input bind:value={newTodo} />
+	<button type="submit">Add</button>
+</form>
 
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
 
-	<Counter />
-</section>
+<div class="columns">
+	<div>
+		<h2>Todos</h2>
+		<ul>
+			{#each outstanding as todo (todo.id)}
+				<li class="contents" animate:flip in:send={{key: todo.id}} out:receive={{key: todo.id}}>
+					<Todo bind:todo={todo}></Todo>
+				</li>
+			{/each}
+		</ul>
+	</div>
+	<div>
+		<h2>Completed</h2>
+		<ul>
+			{#each completed as todo (todo.id)}
+				<li class="contents" animate:flip in:send={{key: todo.id}} out:receive={{key: todo.id}}>
+					<Todo bind:todo={todo}></Todo>
+				</li>
+			{/each}
+		</ul>
+	</div>
+</div>
 
 <style>
-	section {
+	ul {
+		padding: 0;
+	}
+
+	li {
+		background-color: white;
+		margin-bottom: 1em;
+		border: thin solid gray;
 		display: flex;
-		flex-direction: column;
-		justify-content: center;
+		box-shadow: 2px 2px 5px;
+		flex-direction: row;
 		align-items: center;
-		flex: 0.6;
+		list-style-type: none;
+		padding: 0.5em;
+		border-radius: 5px;
 	}
 
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
+	.columns {
+		display: flex;
+		flex-direction: row;
+		gap: 1em;
 	}
 </style>
---
+++ src/routes/Todo.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { slide, crossfade } from 'svelte/transition';
+	import { flip } from 'svelte/animate';
+
+    /** @type {{name: string, completed: boolean, id: number}} */
+    export let todo
+
+</script>
+
+<div>
+    <input bind:value={todo.name} />
+    <input bind:checked={todo.completed} type="checkbox" />
+</div>
+
+<style>
+    input {
+        border: none;
+    }
+</style>
